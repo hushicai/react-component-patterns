71826b6b3ce78ace0ed5033b663c8fc020386aeb
hushicai <bluthcy@gmail.com>
2022-03-26 21:33:38 +0800

    默认属性

--- src/Button.tsx
+++ src/Button.tsx
@@ -1,13 +1,51 @@
 // 无状态组件
 import React, { MouseEvent, FunctionComponent } from "react";
+import withDefaultProps from "./withDefaultProps";
 
 type Props = {
   onClick(e: MouseEvent<HTMLElement>): void;
+  color?: string;
 };
 
 const Button: FunctionComponent<Props> = ({
   onClick: handleClick,
+  color,
   children,
-}) => <button onClick={handleClick}>{children}</button>;
+}) => (
+  <button style={{ color }} onClick={handleClick}>
+    {children}
+  </button>
+);
+
+const defaultProps = {
+  color: "red",
+};
+
+export const ButtonWithDefaultProps = withDefaultProps(defaultProps, Button);
+
+// 函数组件需要显示提供Props，ts无法从参数中推导
+// const ButtonWithDefaultProps = withDefaultProps<Props>(
+//   defaultProps,
+//   ({ onClick: handleClick, color, children }) => (
+//     <button style={{ color }} onClick={handleClick}>
+//       {children}
+//     </button>
+//   ),
+// );
+
+// 类组件无需显示提供Props，ts可以自行推导
+// const ButtonWithDefaultProps = withDefaultProps(
+//   defaultProps,
+//   class Button extends Component<Props> {
+//     render() {
+//       const { onClick: handleClick, color, children } = this.props;
+//       return (
+//         <button style={{ color }} onClick={handleClick}>
+//           {Children}
+//         </button>
+//       );
+//     }
+//   },
+// );
 
 export default Button;
